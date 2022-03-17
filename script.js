9dcc6f9dcd4971ae0319c21ec17d653a488f68d6
brefabu <bunescufabian@gmail.com>
2022-03-17 21:40:49 +0200

    up to date

--- script.js
+++ script.js
@@ -6,33 +6,33 @@ const API = () => {
     switch(APImethod) {
         case "GET":
             axios.get(APIurl).then(res => {
-                $(".response__body").html(res.data);
+                $(".response__body").html(JSON.stringify(res.data, undefined, 2));
             }).catch(err => {
-                $(".response__body").html(err);
+                $(".response__body").html(JSON.stringify(err, undefined, 2));
             })
 
             break;
         case "POST":
             axios.post(APIurl, JSON.parse(APIbody)).then(res => {
-                $(".response__body").html(res.data);
+                $(".response__body").html(JSON.stringify(res.data, undefined, 2));
             }).catch(err => {
-                $(".response__body").html(err);
+                $(".response__body").html(JSON.stringify(err, undefined, 2));
             })
 
             break;
         case "PUT":
             axios.put(APIurl, JSON.parse(APIbody)).then(res => {
-                $(".response__body").html(res.data);
+                $(".response__body").html(JSON.stringify(res.data, undefined, 2));
             }).catch(err => {
-                $(".response__body").html(err);
+                $(".response__body").html(JSON.stringify(err, undefined, 2));
             })
 
             break;
         case "DELETE":
             axios.delete(APIurl).then(res => {
-                $(".response__body").html(res.data);
+                $(".response__body").html(JSON.stringify(res.data, undefined, 2));
             }).catch(err => {
-                $(".response__body").html(err);
+                $(".response__body").html(JSON.stringify(err, undefined, 2));
             })
 
             break;
